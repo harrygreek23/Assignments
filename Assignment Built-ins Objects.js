37153201f12e94f0124c1d42fc24a6a7a0be0a05
Garima <harrygreek23@gmail.com>
2026-08-27 02:56:16 +0530

    project upload

---
+++ Assignment Built-ins Objects.js
@@ -0,0 +1,113 @@
+//Section 1 - Math Object
+//1. Random Number
+// let Rnumber=Math.random();
+// console.log("Random Number:", Rnumber);
+
+
+//2. Random Whole Number
+// let min =1;
+// let max=10;
+// let result = Math.floor(Math.random()*(max-min + 1)) + min;
+// console.log(result)
+
+//3. Round a Number
+    // let roundNum=Math.round(4.6);
+    // console.log(roundNum);
+
+//4. Floor and Ceil
+//    let cnum=Math.ceil(7.3);
+//    let fnum=Math.floor(7.3);
+//     console.log(cnum);
+//     console.log(fnum);
+
+// //5. Absolute Value
+// let positiveNum =Math.abs(-25)
+// console.log(positiveNum);
+
+// 6. Power and Square Root
+//   let num=Math.pow(2,3)
+//    console.log(num);
+// console.log(2 ** 3);//short method
+
+// let squarNum=Math.sqrt(64);
+// console.log(squarNum);
+
+// 7. Minimum and Maximum
+// let smallest = Math.min(10, 25, 5, 18);
+// let largest = Math.max(10, 25, 5, 18);
+
+// console.log("Minimum:", smallest);
+// console.log("Maximum:", largest);
+
+// Section 2 - String Built-in Methods
+// 8. Extract Part of a String
+// str = "JavaScript Programming";
+
+// let trimStr = str.slice(0, 10); 
+
+// console.log(trimStr);
+
+
+// 9. Split a String
+// str = "HTML,CSS,JavaScript";
+
+// let result = str.split(",");
+// console.log(result);
+
+// 10. Replace Text
+// str ="Hello World" ;
+// let result=str.replace("World","JavaScript")
+
+// console.log(result);
+
+
+// 11. Check Email
+// let email = "user@example.com";
+// console.log(email.includes("@"));
+
+//12. Check File Extension
+// str =" Hello JavaScript ";
+// let result= str.trim();
+// console.log('"' + result + '"');
+
+//14. Replace a Greeting
+// let greet = "Hello User";
+// let result = greet.replace("User","Rahul");
+// console.log(result);
+
+//Section 3 - Number Built-in Methods
+//15. Format a Decimal Number
+// let num =12.56789;
+// let result= num.toFixed(2)
+// console.log(result);
+
+// console.log(typeof result);
+
+// 16. Format a Price
+// let price= 99.5;
+// let result= price.toFixed(2)
+// console.log(result);
+
+
+// Section 4 - Date Object
+// 17. Current Date and Time
+// let date =new Date();
+// console.log(date.toLocaleDateString());
+
+// console.log(date.toLocaleTimeString());
+
+//18. Store a Specific Date
+// let date =new Date("2026-01-01");
+// console.log(date);
+
+
+//19. Current Timestamp
+// let date =new Date();
+// console.log(date.toLocaleTimeString());
+
+//20. Simple Date Difference
+// let date1 =new Date("2026-01-01");
+// let date2 =new Date("2027-01-01");
+// let difference = date2 - date1;
+
+// console.log(difference);
